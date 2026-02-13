5d794fc57cd5ab5ea0e48eafe6fdbe6ab8f06e85
SonnaHub <jjahu95@gmail.com>
2026-02-13 17:59:56 +0700

    Update FreeCam.lua

--- FreeCam.lua
+++ FreeCam.lua
@@ -40,18 +40,27 @@ end
 --=====================
 -- LOCK PLAYER (FULL FIX)
 --=====================
+local oldWalkSpeed = 16
+local oldJumpPower = 50
+
 local function lockPlayer(state)
 	local hum = getHumanoid()
 	local root = getRoot()
 
 	if state then
-		hum.PlatformStand = true
+		-- lưu lại chỉ số cũ
+		oldWalkSpeed = hum.WalkSpeed
+		oldJumpPower = hum.JumpPower
+
+		-- khóa di chuyển nhưng vẫn đứng idle bình thường
+		hum.WalkSpeed = 0
+		hum.JumpPower = 0
 		hum.AutoRotate = false
-		root.Anchored = true
 	else
-		hum.PlatformStand = false
+		-- trả lại như cũ
+		hum.WalkSpeed = oldWalkSpeed
+		hum.JumpPower = oldJumpPower
 		hum.AutoRotate = true
-		root.Anchored = false
 	end
 
 	root.AssemblyLinearVelocity = Vector3.zero
